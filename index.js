5cef51322ec8eb621de865bf8dd316303997a481
Alemba <alembakiragu@gmail.com>
2022-06-10 14:54:17 +0300

    commiting changes

---
+++ index.js
@@ -0,0 +1,16 @@
+
+function receivesAFunction(callback) {
+    callback();
+  }
+  
+
+  function returnsANamedFunction() {
+    return function namedFn() {
+      console.log("a named function");
+    };
+  }
+  
+
+  function returnsAnAnonymousFunction() {
+    return () => console.log("an anonymous function");
+  }
